fbed19bc895587078318ea3023bae46bdfba6089
wwIrvingww <mor22781@guvg.edu.gt>
2023-03-05 08:32:35 -0600

    "se creo el main para que el usuario ingrese el tipo de Map que desea usar"

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("-\n---Seleccione la interfaz que desea implementar----\n1.HashMap\n2.TreeMap\n3.LinkedHashMap");
+
+    }
+}
